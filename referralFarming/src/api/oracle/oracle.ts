94cd030b2e7a6cfcb4e0def6908b5a4709182b25
Mariia <msynoverska@webinerds.com>
2022-07-20 19:28:46 +0100

    refactor: oracle calls

--- referralFarming/src/api/oracle/oracle.ts
+++ referralFarming/src/api/oracle/oracle.ts
@@ -6,7 +6,7 @@ import { TNodeUrl } from '../discovery';
 
 import { buffer, address } from 'utils';
 
-const iface = new Interface([
+const ifaceReferralFarmsV1Reactor = new Interface([
   'function getLastConfirmationReward(bytes32 farmHash) view returns (uint128)',
   'function getFarmTokenSize(bytes32 farmHash, bytes24 referredToken) view returns (uint128)',
   'function getFarmTrackedRewardValue(bytes32 farmHash) view returns (uint128)',
@@ -18,9 +18,10 @@ export async function getLastConfirmationReward(
 ): Promise<bigint> {
   const reactorAddress = address.toReactorAddress('referralFarmsV1Reactor');
 
-  const data = iface.encodeFunctionData('getLastConfirmationReward', [
-    farmHash,
-  ]);
+  const data = ifaceReferralFarmsV1Reactor.encodeFunctionData(
+    'getLastConfirmationReward',
+    [farmHash],
+  );
 
   return rpc
     .call<{ to: string; data: string }[], string>(
@@ -29,36 +30,6 @@ export async function getLastConfirmationReward(
       [
         {
           to: buffer.toHex(reactorAddress),
-          data,
-        },
-      ],
-    )
-    .then((res) => BigInt(res?.result || '0'))
-    .catch(() => BigInt('0'));
-}
-
-// TODO: add real value
-const hardcodedFarmTokenSizeV1Reactor =
-  '0xd02d27c595ea215f9c55eb8ee0c70b78c367935e';
-export async function getFarmTokenSize(
-  farmHash: FarmHash,
-  referredToken: ChainAddress,
-  host: TNodeUrl,
-): Promise<bigint> {
-  const data = iface.encodeFunctionData('getFarmTokenSize', [
-    farmHash,
-    referredToken,
-  ]);
-  // const reactorAddress = address.toReactorAddress('referralFarmsV1Reactor');
-
-  return rpc
-    .call<{ to: string; data: string }[], string>(
-      host + RpcRoute.rpc,
-      RpcOracleMethod.oracle_call,
-      [
-        {
-          to: hardcodedFarmTokenSizeV1Reactor,
-          // to: buffer.toHex(address.toReactorAddress('farmTokenSizeV1Reactor')),
           data,
         },
       ],
@@ -73,9 +44,10 @@ export async function getFarmsTrackedRewardsValue(
 ): Promise<bigint> {
   const reactorAddress = address.toReactorAddress('referralFarmsV1Reactor');
 
-  const data = iface.encodeFunctionData('getFarmTrackedRewardValue', [
-    farmHash,
-  ]);
+  const data = ifaceReferralFarmsV1Reactor.encodeFunctionData(
+    'getFarmTrackedRewardValue',
+    [farmHash],
+  );
 
   return rpc
     .call<{ to: string; data: string }[], string>(
@@ -84,6 +56,38 @@ export async function getFarmsTrackedRewardsValue(
       [
         {
           to: buffer.toHex(reactorAddress),
+          data,
+        },
+      ],
+    )
+    .then((res) => BigInt(res?.result || '0'))
+    .catch(() => BigInt('0'));
+}
+
+const ifaceFarmTokenSizeV1Reactor = new Interface([
+  'function getFarmTokenSize(bytes32 farmHash, bytes24 referredToken) view returns (uint128)',
+]);
+// TODO: add real value
+const hardcodedFarmTokenSizeV1Reactor =
+  '0xd02d27c595ea215f9c55eb8ee0c70b78c367935e';
+export async function getFarmTokenSize(
+  farmHash: FarmHash,
+  referredToken: ChainAddress,
+  oracleUrl: TNodeUrl,
+): Promise<bigint> {
+  const data = ifaceFarmTokenSizeV1Reactor.encodeFunctionData(
+    'getFarmTokenSize',
+    [farmHash, referredToken],
+  );
+
+  return rpc
+    .call<{ to: string; data: string }[], string>(
+      oracleUrl + RpcRoute.rpc,
+      RpcOracleMethod.oracle_call,
+      [
+        {
+          to: hardcodedFarmTokenSizeV1Reactor,
+          // to: buffer.toHex(address.toReactorAddress('farmTokenSizeV1Reactor')),
           data,
         },
       ],
